d894329afcafd4382e51e8eb1251023941c5451e
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-13 22:40:11 +0800

    I have added some functionalities, such as, show the product with quantities and prices in the troller, show the total o pay, and etc... I'll finish tomorrow.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
+let valorTotal = 0
+document.getElementById('valor-total').textContent = 'R$ 0'
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('quantidade').value = 0
+
+
+
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
-    let valorProduto = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
+    let valorProduto = parseInt(produto.split('R$')[1]);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let multProduto = quantidade * valorProduto;
+
+    // agora preciso fazer a soma dos produtos
 
     
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${multProduto}</span>
+        </section>`
+
+    valorTotal += multProduto;
+
+    let printTotal = document.getElementById('valor-total');
+    printTotal.textContent = `R$ ${valorTotal}`;
 
 }
 
