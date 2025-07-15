d8195c98c25c26a9a75f5abd382cb34737a2c72d
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-15 15:12:05 +0800

    I have done this project, uhuuulll. Done one more little project here. Coming for the success.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 let valorTotal = 0
-document.getElementById('valor-total').textContent = 'R$ 0'
-document.getElementById('lista-produtos').innerHTML = '';
-document.getElementById('quantidade').value = 0
-
+limpar()
 
 
 function adicionar() {
+
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorProduto = parseInt(produto.split('R$')[1]);
     let quantidade = parseInt(document.getElementById('quantidade').value);
-    let multProduto = quantidade * valorProduto;
 
     // agora preciso fazer a soma dos produtos
 
-    
+    let multProduto = quantidade * valorProduto;
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML += `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${multProduto}</span>
@@ -24,9 +21,15 @@ function adicionar() {
 
     let printTotal = document.getElementById('valor-total');
     printTotal.textContent = `R$ ${valorTotal}`;
+    document.getElementById('quantidade').value = 1;
+    }
 
-}
+
+
+
 
 function limpar() {
-    
+    document.getElementById('valor-total').textContent = 'R$ 0'
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('quantidade').value = 1
 }
